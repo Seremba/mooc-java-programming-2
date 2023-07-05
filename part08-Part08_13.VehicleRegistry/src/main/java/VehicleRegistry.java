6f14e909ea70de02e2fcb6075660c34642da5670
Seremba Patrick <pserembae@gmail.com>
2023-07-05 13:18:05 +0300

    expand VehicleRegistery class

--- part08-Part08_13.VehicleRegistry/src/main/java/VehicleRegistry.java
+++ part08-Part08_13.VehicleRegistry/src/main/java/VehicleRegistry.java
@@ -1,13 +1,60 @@
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author seremba
  */
 public class VehicleRegistry {
-    
+
+    private HashMap<LicensePlate, String> registry;
+
+    public VehicleRegistry() {
+        this.registry = new HashMap<>();
+    }
+
+    public boolean add(LicensePlate licensePlate, String owner) {
+
+        if (!registry.containsKey(licensePlate)) {
+            this.registry.put(licensePlate, owner);
+            return true;
+        }
+        return false;
+    }
+
+    public String get(LicensePlate licensePlate) {
+        return this.registry.getOrDefault(licensePlate, null);
+    }
+
+    public boolean remove(LicensePlate licensePlate) {
+        if (this.registry.containsKey(licensePlate)) {
+            this.registry.remove(licensePlate);
+            return true;
+        }
+        return false;
+    }
+
+    public void printLicensePlates() {
+        for (LicensePlate plate : this.registry.keySet()) {
+            System.out.println(plate);
+        }
+    }
+
+    public void printOwners() {
+        ArrayList<String> owners = new ArrayList<>();
+        
+        for (String owner : this.registry.values()) {
+            if(!owners.contains(owner)){
+                System.out.println(owner);
+                owners.add(owner);
+            }        
+            
+        }
+    }
 }
