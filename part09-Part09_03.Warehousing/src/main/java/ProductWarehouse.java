2587652a72fd705f21c91e4043a48717192ee193
Seremba Patrick <pserembae@gmail.com>
2023-07-13 22:10:40 +0300

    add toString() and setName methods

--- part09-Part09_03.Warehousing/src/main/java/ProductWarehouse.java
+++ part09-Part09_03.Warehousing/src/main/java/ProductWarehouse.java
@@ -8,6 +8,24 @@
  *
  * @author seremba
  */
-public class ProductWarehouse {
-    
+public class ProductWarehouse extends Warehouse {
+
+    private String name;
+
+    public ProductWarehouse(String name, double capacity) {
+        super(capacity);
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
+    public String toString() {
+        return this.name + ": " + super.toString();
+    }
 }
