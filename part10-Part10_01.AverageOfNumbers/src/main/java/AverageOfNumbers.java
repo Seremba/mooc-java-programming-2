c3ace86d387e4e29bf230217904f0eae991fcfe2
Seremba Patrick <pserembae@gmail.com>
2023-07-17 14:03:14 +0300

    find average of numbers using stream

--- part10-Part10_01.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ part10-Part10_01.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -6,7 +6,19 @@ public class AverageOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> numbers = new ArrayList<>();
         // Write your program here
+        System.out.println("Input numbers, type \"end\" to stop.");
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("end")) {
+                break;
+            }
+            numbers.add(input);
+        }
+
+        double average = numbers.stream().mapToInt(s -> Integer.valueOf(s)).average().getAsDouble();
+        System.out.println("average of the numbers: " + average);
 
     }
 }
