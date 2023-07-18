eb365c2752573a6892f14e7d46c223a7f867618d
Seremba Patrick <pserembae@gmail.com>
2023-07-18 09:07:11 +0300

    use stream to collect positive numbers

--- part10-Part10_03.PositiveNumbers/src/main/java/PositiveNumbers.java
+++ part10-Part10_03.PositiveNumbers/src/main/java/PositiveNumbers.java
@@ -11,4 +11,10 @@ public class PositiveNumbers {
 
     }
 
+    public static List<Integer> positive(List<Integer> numbers) {
+        return numbers.stream()
+                .filter(value -> value > 0)
+                .collect(Collectors.toList());
+    }
+
 }
