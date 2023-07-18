d6c6ee799161d651da33e82063ff3d63e21a4dd1
Seremba Patrick <pserembae@gmail.com>
2023-07-18 10:13:20 +0300

    print user input

--- part10-Part10_05.PrintingUserInput/src/main/java/PrintingUserInput.java
+++ part10-Part10_05.PrintingUserInput/src/main/java/PrintingUserInput.java
@@ -6,6 +6,16 @@ public class PrintingUserInput {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> words = new ArrayList<>();
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("")) {
+                break;
+            }
+            words.add(input);
+        }
+
+        words.stream().forEach(word -> System.out.println(word));
 
     }
 }
