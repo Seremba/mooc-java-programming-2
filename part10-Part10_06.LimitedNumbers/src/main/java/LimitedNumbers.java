9767a002763e3f57f28602e759395426fd9144e8
Seremba Patrick <pserembae@gmail.com>
2023-07-18 10:37:09 +0300

    print numbers between 1 - 5

--- part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
+++ part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
@@ -6,6 +6,17 @@ public class LimitedNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> numbers = new ArrayList<>();
+        while (true) {
+            int number = Integer.valueOf(scanner.nextLine());
+            if (number < 0) {
+                numbers.stream()
+                        .filter(num -> num >= 1 && num <= 5)
+                        .forEach(val -> System.out.println(val));
+                return;
+            }
+            numbers.add(number);
+        }
 
     }
 }
