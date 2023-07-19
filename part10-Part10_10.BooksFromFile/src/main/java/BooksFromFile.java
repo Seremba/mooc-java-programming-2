52de7997717fd00223976ecff44f9d19db2f1857
Seremba Patrick <pserembae@gmail.com>
2023-07-19 22:54:10 +0300

    add books to a file

--- part10-Part10_10.BooksFromFile/src/main/java/BooksFromFile.java
+++ part10-Part10_10.BooksFromFile/src/main/java/BooksFromFile.java
@@ -7,11 +7,25 @@ import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class BooksFromFile {
-    
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // test your method here
 
+    }
+
+    public static List<Book> readBooks(String file) {
+        List<Book> books = new ArrayList<>();
+        try {
+            Files.lines(Paths.get(file))
+                    .map(val -> val.split(","))
+                    .filter(parts -> parts.length >= 4)
+                    .map(book -> new Book(book[0], Integer.valueOf(book[1]), Integer.valueOf(book[2]), book[3]))
+                    .forEach(book -> books.add(book));
+        } catch (Exception e) {
+            System.out.println("Error " + e.getMessage());
+        }
+        return books;
     }
 
 }
