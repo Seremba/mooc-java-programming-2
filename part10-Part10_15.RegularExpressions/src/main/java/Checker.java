5cb96cfa730da9c61e86d744ca738d5b0dbbe973
Seremba Patrick <pserembae@gmail.com>
2023-07-23 11:16:38 +0300

    regular expression for day of week

--- part10-Part10_15.RegularExpressions/src/main/java/Checker.java
+++ part10-Part10_15.RegularExpressions/src/main/java/Checker.java
@@ -1,5 +1,14 @@
 
-
 public class Checker {
 
+    public boolean isDayOfWeek(String string) {
+        String pattern = "^(mon|tue|wed|thu|fri|sat|sun)$";
+
+        if (string.matches(pattern)) {
+            return true;
+        }
+        return false;
+    }
+
+   
 }
