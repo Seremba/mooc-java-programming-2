a92e31f39aae1573ee4f2ea071aa58b90357fc8a
Seremba Patrick <pserembae@gmail.com>
2023-07-24 11:47:27 +0300

    create Hand class

---
+++ part10-Part10_17.SortThemCards/src/main/java/Hand.java
@@ -0,0 +1,31 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author seremba
+ */
+public class Hand {
+
+    private List<Card> cards;
+
+    public Hand() {
+        this.cards = new ArrayList<>();
+    }
+
+    public void add(Card card) {
+        this.cards.add(card);
+    }
+
+    public void print() {
+        for (Card card : this.cards) {
+            System.out.println(card);
+        }
+    }
+}
