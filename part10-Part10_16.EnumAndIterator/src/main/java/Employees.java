7fd8b9a46c098394a45ea5fe25077793579cc800
Seremba Patrick <pserembae@gmail.com>
2023-07-24 11:04:23 +0300

    add method for removing an employee depending on education

--- part10-Part10_16.EnumAndIterator/src/main/java/Employees.java
+++ part10-Part10_16.EnumAndIterator/src/main/java/Employees.java
@@ -13,33 +13,42 @@ import java.util.List;
  * @author seremba
  */
 public class Employees {
-
+    
     private List<Person> employess;
-
+    
     public Employees() {
         this.employess = new ArrayList<>();
     }
-
+    
     public void add(Person personToAdd) {
         this.employess.add(personToAdd);
     }
-
+    
     public void add(List<Person> peopleToAdd) {
         this.employess.addAll(peopleToAdd);
     }
-
+    
     public void print() {
         Iterator<Person> iterator = employess.iterator();
         while (iterator.hasNext()) {
             System.out.println(iterator.next());
         }
     }
-
+    
     public void print(Education education) {
         Iterator<Person> iterator = employess.iterator();
         while (iterator.hasNext()) {
             if (iterator.next().getEducation().equals(education)) {
                 System.out.println(iterator.next());
+            }
+        }
+    }
+
+    public void fire(Education education) {
+        Iterator<Person> iterator = employess.iterator();
+        while (iterator.hasNext()) {
+            if (iterator.next().getEducation().equals(education)) {
+                iterator.remove();
             }
         }
     }
