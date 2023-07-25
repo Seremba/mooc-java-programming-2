1dd2a245cfcad053577d6b3a84935e98eda4f522
Seremba Patrick <pserembae@gmail.com>
2023-07-25 12:29:23 +0300

    create class Person and Interface Saveable according to class diagrams

---
+++ part11-Part11_06.SaveablePerson/src/main/java/Person.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author seremba
+ */
+public class Person implements Saveable{
+   private String name; 
+   private String address;
+
+    @Override
+    public void save() {
+         System.out.println("Save");
+    }
+
+    @Override
+    public void delete() {
+         System.out.println("Delete");
+    }
+
+    @Override
+    public void load(String address) {
+        System.out.println("Address " + address);
+    }
+}
