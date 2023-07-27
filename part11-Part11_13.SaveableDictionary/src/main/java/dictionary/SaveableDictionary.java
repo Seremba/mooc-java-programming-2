4c4d5a10823462b0f0638abd2b7d2531d33c42ce
Seremba Patrick <pserembae@gmail.com>
2023-07-27 20:01:38 +0300

    create SavableDictionary class, add methods for adding word and translating the word

---
+++ part11-Part11_13.SaveableDictionary/src/main/java/dictionary/SaveableDictionary.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dictionary;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author seremba
+ */
+public class SaveableDictionary {
+
+    private Map<String, String> translateToFinish;
+    private Map<String, String> translateFromFinish;
+
+    public SaveableDictionary() {
+        this.translateToFinish = new HashMap<>();
+        this.translateFromFinish = new HashMap<>();
+    }
+
+    public void add(String words, String translation) {
+        this.translateToFinish.put(words, translation);
+        this.translateFromFinish.put(translation, words);
+    }
+
+    public String translate(String word) {
+        String translation = null;
+        if (this.translateToFinish.containsKey(word)) {
+            translation = this.translateToFinish.get(word);
+        }
+        if (this.translateFromFinish.containsKey(word)) {
+            translation = this.translateFromFinish.get(word);
+        }
+        return translation;
+    }
+}
