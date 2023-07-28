750e8234bdcac13ac6578092201b4e1b01863ba8
Seremba Patrick <pserembae@gmail.com>
2023-07-28 10:34:02 +0300

    add takeFromPipe() method

--- part12-Part12_02.Pipe/src/main/java/Pipe.java
+++ part12-Part12_02.Pipe/src/main/java/Pipe.java
@@ -1,13 +1,39 @@
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author seremba
  */
-public class Pipe {
-    
+public class Pipe<T> {
+
+    private ArrayList<T> values;
+
+    public Pipe() {
+        this.values = new ArrayList<>();
+    }
+
+    public void putIntoPipe(T value) {
+        this.values.add(value);
+    }
+
+    public T takeFromPipe() {
+        if (this.values.isEmpty()) {
+            return null;
+        }
+        Iterator<T> iterator = values.iterator();
+
+        T value = null;
+        while (iterator.hasNext()) {
+            value = iterator.next();
+            iterator.remove();
+        }
+        return value;
+    }
 }
