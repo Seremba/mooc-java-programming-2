3efe01e146e11bdc1f36121a0f56e70461e10092
Seremba Patrick <pserembae@gmail.com>
2023-07-30 17:50:07 +0300

    implement the add method of the list

--- part12-Part12_04.List/src/main/java/List.java
+++ part12-Part12_04.List/src/main/java/List.java
@@ -8,6 +8,30 @@
  *
  * @author seremba
  */
-public class List {
-    
+public class List<T> {
+
+    private T[] values;
+    private int emptyIndex;
+
+    public List() {
+        this.values = (T[]) new Object[10];
+        this.emptyIndex = 0;
+    }
+
+    public void add(T value) {
+        if (this.emptyIndex == this.values.length) {
+            grow();
+        }
+        this.values[emptyIndex] = value;
+        this.emptyIndex++;
+    }
+
+    private void grow() {
+        int newSize = this.values.length + this.values.length / 2;
+        T[] newValues = (T[]) new Object[newSize];
+        for (int i = 0; i < newValues.length; i++) {
+            newValues[i] = this.values[i];
+        }
+        this.values = newValues;
+    }
 }
