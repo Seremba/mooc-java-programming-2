178dfe4ab5bc4830de6685b77eb3a2f5c7b5be86
Seremba Patrick <pserembae@gmail.com>
2023-07-31 10:59:14 +0300

    add methods value and size to the list

--- part12-Part12_04.List/src/main/java/List.java
+++ part12-Part12_04.List/src/main/java/List.java
@@ -36,12 +36,7 @@ public class List<T> {
     }
 
     public boolean contains(T value) {
-        for (int i = 0; i < this.firstFreeIndex; i++) {
-            if (this.values[i].equals(value)) {
-                return true;
-            }
-        }
-        return false;
+        return this.indexOfValue(value) >= 0;
     }
 
     public void remove(T value) {
@@ -66,5 +61,17 @@ public class List<T> {
         for (int i = 0; i < this.firstFreeIndex - 1; i++) {
             this.values[i] = this.values[i + 1];
         }
+    }
+
+    public int size() {
+        return this.firstFreeIndex;
+    }
+
+    public T value(int index) {
+        if (index < 0 || index >= this.firstFreeIndex) {
+            throw new ArrayIndexOutOfBoundsException("Index " + index + " outside of [0, " + this.firstFreeIndex + "]");
+        }
+
+        return this.values[index];
     }
 }
