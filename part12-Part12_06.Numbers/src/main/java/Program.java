16b28e514a8c145e02a76762ca471dc3367e47a1
Seremba Patrick <pserembae@gmail.com>
2023-08-01 18:13:32 +0300

    generate random numbers

--- part12-Part12_06.Numbers/src/main/java/Program.java
+++ part12-Part12_06.Numbers/src/main/java/Program.java
@@ -6,6 +6,13 @@ public class Program {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("How many random numbers should be printed?");
+        int number = Integer.valueOf(scanner.nextLine());
+        Random rand = new Random();
+        for (int i = 0; i < number; i++) {
+            int num = rand.nextInt(11);
+            System.out.println(num);
+        }
     }
 
 }
