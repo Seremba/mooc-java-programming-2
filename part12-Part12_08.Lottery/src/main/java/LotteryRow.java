dea184454873753458adf02f0820ce6e9d0ddf0f
Seremba Patrick <pserembae@gmail.com>
2023-08-02 13:16:31 +0300

    generate random numbers and add them to the list

--- part12-Part12_08.Lottery/src/main/java/LotteryRow.java
+++ part12-Part12_08.Lottery/src/main/java/LotteryRow.java
@@ -5,9 +5,11 @@ import java.util.Random;
 public class LotteryRow {
 
     private ArrayList<Integer> numbers;
+    private Random random;
 
     public LotteryRow() {
         // Draw the numbers when the LotteryRow is created
+        this.random = new Random();
         this.randomizeNumbers();
     }
 
@@ -16,15 +18,23 @@ public class LotteryRow {
     }
 
     public void randomizeNumbers() {
-        // Initialize the list for numbers
+
         this.numbers = new ArrayList<>();
-        // Implement the random number generation here
-        // the method containsNumber is probably useful
+
+        while(this.numbers.size() < 7){
+            int randomNumber = this.random.nextInt(40) + 1;
+            if(!this.numbers.contains(randomNumber)) {
+                this.numbers.add(randomNumber);
+            }
+        }
+
     }
 
     public boolean containsNumber(int number) {
         // Check here whether the number is among the drawn numbers
+        if (this.numbers.contains(number)) {
+            return true;
+        }
         return false;
     }
 }
-
